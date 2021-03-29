12fcca2d1d66097a7a0fc5561caad2a551a08fd7
vdutor <dutordoirv@gmail.com>
2021-03-29 17:40:05 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+name = 'gspheres'
+
+setup(
+    name="GPs on Spheres",
+    version="0.01",
+    author="vd309@cam.ac.uk",
+    description="Gaussian processes on Spheres",
+    packages=[name],
+)
