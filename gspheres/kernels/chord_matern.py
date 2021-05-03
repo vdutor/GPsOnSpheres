2dfab79b8707ee6f3bd90fafcf6df5987830f79e
Vincent Dutordoir <dutordoirv@gmail.com>
2021-05-03 10:25:12 +0100

    Remove dimensionality assert in chord kernel

--- gspheres/kernels/chord_matern.py
+++ gspheres/kernels/chord_matern.py
@@ -13,8 +13,6 @@ from ..utils import surface_area_sphere
 
 class ChordMatern(gpflow.kernels.Kernel):
     def __init__(self, nu: int, dimension: int):
-        assert dimension == 3
-
         if nu == 1 / 2:
             self.base_kernel = gpflow.kernels.Matern12()
         elif nu == 3 / 2:
