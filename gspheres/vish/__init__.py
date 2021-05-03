bf6cbc0b706674e63d9ae08b9ce559db66809373
Saad Hamid <saadhamid96@gmail.com>
2021-05-03 16:47:17 +0100

    Add log-transform implementation

--- gspheres/vish/__init__.py
+++ gspheres/vish/__init__.py
@@ -20,12 +20,14 @@ def map_to_sphere(X: np.ndarray, bias: Union[int, float]) -> np.ndarray:
 
 
 class WsabiLSVGP(SVGP):
-    """SVGP with a square root transform."""
+    """SVGP that should be fit to the log transform of the training
+    targets. The unwarping is done by linearisation.
+    """
     def __init__(self, *args, alpha=0, **kwargs):
         super().__init__(*args, **kwargs)
         self.alpha = alpha
 
-    def predict_f_sq(
+    def predict_unwarped_f(
             self, Xnew: InputData, full_cov=False, full_output_cov=False
     ) -> MeanAndVariance:
         mean, var = self.predict_f(Xnew, full_cov, full_output_cov)
@@ -39,3 +41,26 @@ class WsabiLSVGP(SVGP):
 
         return f_sq_mean, f_sq_var
 
+
+class LogMSVGP(SVGP):
+    """SVGP that should be fit to the log transform of the training
+    targets. The unwarping is done by moment matching.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def predict_unwarped_f(
+            self, Xnew: InputData, full_cov=False, full_output_cov=False
+    ) -> MeanAndVariance:
+        mean, var = self.predict_f(Xnew, full_cov, full_output_cov)
+
+        f_exp_mean = tf.exp(mean + 0.5 * var)
+        exp_var = tf.exp(var) - 1
+        if full_cov:
+            covar_factors = tf.linalg.matmul(mean, mean, transpose_a=True)
+            f_exp_var = covar_factors * exp_var
+        else:
+            f_exp_var = exp_var * mean ** 2
+
+        return f_exp_mean, f_exp_var
