Fix spherical_matern kernel so that posterior variance drops to approximately the likelihood variance at data points. ChordMatern is still broken in this regard.

gspheres/kernels/chord_matern.py
@@ -27,14 +27,9 @@ class ChordMatern(gpflow.kernels.Kernel):
         self.dimension = dimension
         self._training = True
         self._eigenvalues = {}
-
-    @property
-    def training(self):
-        return self._training
-
-    @training.setter
-    def training(self, flag: bool):
-        self._training = flag
+        self._cache_parameters = {
+            'variance': self.variance, 'lengthscales': self.lengthscales
+        }
 
     @property
     def variance(self):
@@ -66,25 +61,38 @@ class ChordMatern(gpflow.kernels.Kernel):
             values.append(tf.reshape(v, shape=[-1]))
         return tf.concat(values, axis=0)
 
-    def eigenvalues(self, max_degree: int) -> tf.Tensor:
-        if self.training:
-            # Make sure cache is empty, as lengthscale and variance are
-            # about to be updated.
-            self._eigenvalues = {}
-            return self._compute_eigenvalues(max_degree)
-        else:
-            # self._eigenvalues is a dictionary (that functions as a cache)
-            if max_degree not in self._eigenvalues:
-                self._eigenvalues[max_degree] = self._compute_eigenvalues(
-                    max_degree
+    def verify_eigenvalue_cache(self):
+        """Check that lengthscale and variance have not been changed.
+
+        If they have been changed, clear the cache.
+        """
+        variance = self.variance.numpy().item()
+        lengthscale = self.lengthscales.numpy().item()
+        if self._cache_parameters['variance'] != variance:
+            for max_degree in self._eigenvalues:
+                self._eigenvalues[max_degree] *= (
+                        self.variance / self._cache_parameters['variance']
                 )
-            return self._eigenvalues[max_degree]
+        if self._cache_parameters['lengthscales'] != lengthscale:
+            self._eigenvalues = {}
+        self._cache_parameters['variance'] = variance
+        self._cache_parameters['lengthscales'] = lengthscale
+
+    def eigenvalues(self, max_degree: int) -> tf.Tensor:
+        self.verify_eigenvalue_cache()
+        if max_degree not in self._eigenvalues:
+            self._eigenvalues[max_degree] = self._compute_eigenvalues(
+                max_degree
+            )
+        return self._eigenvalues[max_degree]
 
     def K(self, X: TensorType, X2: Optional[TensorType] = None) -> tf.Tensor:
+        # TODO: Refactor in terms of truncated Mercer decomposition.
         return self.base_kernel.K(X, X2)
 
     def K_diag(self, X: TensorType) -> tf.Tensor:
         """ Approximate the true kernel by an inner product between feature functions. """
+        # TODO: Refactor in terms of truncated Mercer decomposition.
         return self.base_kernel.K_diag(X)
 
 

gspheres/kernels/spherical_matern.py
@@ -1,19 +1,23 @@
 from typing import Optional, Union
 
+import math
 import numpy as np
 import tensorflow as tf
 from scipy.special import gamma
 
 import gpflow
 from gpflow.base import TensorType
+from gpflow import covariances as cov
 
+from gspheres.spherical_harmonics import SphericalHarmonics
 from ..fundamental_set import num_harmonics
 from ..gegenbauer_polynomial import Gegenbauer
 
 
 class SphericalMatern(gpflow.kernels.Kernel):
-    def __init__(self, nu: float, truncation_level: int, dimension: int):
+    def __init__(self, nu: float, degrees: int, dimension: int):
         """
+        :param degrees: Max degree for spherical harmonics.
         :param dimension: S^{d-1}, R^d with d = dimension
         """
         assert nu in [1 / 2, 3 / 2, 5 / 2]
@@ -25,36 +29,35 @@ class SphericalMatern(gpflow.kernels.Kernel):
         self.dimension = dimension
         self.alpha = (dimension - 2) / 2
         self.nu = nu
-        self.truncation_level = truncation_level  # = L
-        self.Cs = [Gegenbauer(n, self.alpha) for n in range(self.truncation_level)]
+        self.spherical_harmonics = SphericalHarmonics(
+            dimension=dimension, degrees=degrees
+        )
+        # Truncation level is number of spherical harmonics.
+        self.degrees = degrees
+        self.truncation_level = np.sum(
+            [num_harmonics(3, d) for d in range(degrees)]
+        ).item()
+        self.Cs = [Gegenbauer(n, self.alpha) for n in range(self.degrees)]
         self.lengthscales = gpflow.Parameter(1.0, transform=gpflow.utilities.positive())
 
-        _eigenvals = self.eigenvalues(self.truncation_level)
+        _eigenvals = self.eigenvalues(self.degrees)
         self.constants = tf.convert_to_tensor(
             [
                 eigenvalue * (n + self.alpha) / self.alpha
-                for eigenvalue, n in zip(_eigenvals, range(self.truncation_level))
+                for eigenvalue, n in zip(_eigenvals, range(self.degrees))
             ]
         )  # [L]
         self.diag = tf.reduce_sum(
             [
                 num_harmonics(self.dimension, n) * eigenvalue
-                for eigenvalue, n in zip(_eigenvals, range(self.truncation_level))
+                for eigenvalue, n in zip(_eigenvals, range(self.degrees))
             ]
         )  # scalar
         self.variance = gpflow.Parameter(1.0, transform=gpflow.utilities.positive())
-        self._training = True
-
-    @property
-    def training(self):
-        return self._training
-
-    @training.setter
-    def training(self, flag: bool):
-        self._training = flag
 
     def eigenvalues(self, max_degree: int):
         ns = tf.convert_to_tensor(np.arange(max_degree), dtype=gpflow.default_float())
+        # Eigenvalues of the Laplace-Beltrami operator.
         eigenvalues_harmonics = tf.convert_to_tensor(ns * (ns + self.dimension - 2))  # [L]
         return (
             self.spectral_density(eigenvalues_harmonics ** 0.5) / self.normalisation_constant

gspheres/vish/eigenfunction_variables.py
@@ -48,8 +48,8 @@ def Kuf_sphericalmatern_sphericalharmonicfeatures(
 ):
     """
     Covariance between spherical harmonic features and function values.
-
     """
+
     return tf.transpose(inducing_variable.spherical_harmonics(X))
 
 